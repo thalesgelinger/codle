1480388cccaaa59a9aafec373d418a5f4089fea1
Thales Gelinger <thalesgelinger@gmail.com>
2024-02-10 18:42:16 -0300

    feat: add way to build with a new challenge

---
+++ src/challenge/index.js
@@ -0,0 +1,13 @@
+// THIS IS AUT GENERATED DON'T TOUCH IT
+export const challenge = {
+    title: "Placeholder",
+    description: "Description for the challenge",
+    example: "Some examples",
+    requirement: "What the code should do",
+    tests: runTests.toString()
+}
+
+
+function runTests() {
+    if (true) error("Just a sample this should never run!")
+}
